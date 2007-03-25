7605d080d8b7c39cf9b46d5eb70fc819865233e7
leonb <leonb>
2007-03-25 21:02:12 +0000

    updated notices for license change in djvulibre 3.5.19 (either version 2 of the gpl or (at your option) any later version)

--- src/djview.cpp
+++ src/djview.cpp
@@ -4,7 +4,8 @@
 //C- Copyright (c) 2006  Leon Bottou
 //C-
 //C- This software is subject to, and may be distributed under, the
-//C- GNU General Public License. The license should have
+//C- GNU General Public License, either version 2 of the license,
+//C- or (at your option) any later version. The license should have
 //C- accompanied the software or you may obtain a copy of the license
 //C- from the Free Software Foundation at http://www.fsf.org .
 //C-
