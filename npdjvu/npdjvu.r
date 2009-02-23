ca2c02eec21b968303904957e44e8f670981dafb
leonb <leonb>
2009-02-23 22:21:32 +0000

    Added auxilliary resource files (to debug)

---
+++ npdjvu/npdjvu.r
@@ -0,0 +1,28 @@
+#define SystemSevenOrLater 1
+
+#include <Carbon/Carbon.r>
+
+resource 'STR#' (127)
+{{
+    "DjVu File",
+    "DjVu File",
+    "DjVu File",
+    "DjVu File (obsolete)",
+    "DjVu File (obsolete)",
+    "DjVu File"
+}};
+
+resource 'STR#' (128)
+{{
+    "image/x-djvu", "djvu,djv",
+    "image/x.djvu", "",
+    "image/djvu", "",
+    "image/x-dejavu", "",
+    "image/x-iw44", "",
+    "image/vnd.djvu", ""
+}};
+
+resource 'STR#' (126)
+{{
+  "The DjView4 browser plugin"
+}};
