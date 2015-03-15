2a0ce7721da3be808d46cb22ad08c8b8f58721ce
Leon Bottou <leon@bottou.org>
2015-03-15 15:56:03 -0400

    Can create self-contained app bundle for macosx10.9 (with doc)

--- mac/make_djview_dmg.sh
+++ mac/make_djview_dmg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+dmgname="DjVuLibre-3.5.27+DjView-4.10-intel64"
+
 cd $(dirname $0)
 if ! test -d DjView.app ; then
   echo 2>&1 "Run 'make_djview_bundle.sh' first".
@@ -26,5 +28,5 @@ run cp -r DjView.app $dmg || exit
 run cp ReadMe.rtf $dmg || exit
 book=DjView.app/Contents/share/doc/djvu/djvulibre-book-en.djvu
 run ln -s $book $dmg/Manual.djvu || exit
-run hdiutil create -ov -srcfolder $dmg DjView.dmg || exit
+run hdiutil create -ov -srcfolder $dmg -volname "$dmgname" "$dmgname".dmg || exit
 run rm -rf $dmg
