f2a9f43d338ec06e24f76accfacb868159b4489f
Leon Bottou <leon@bottou.org>
2015-03-15 22:48:51 -0400

    fix configure script. make viewport native (fixing mac issues)

--- src/qdjvuwidget.cpp
+++ src/qdjvuwidget.cpp
@@ -2010,10 +2010,6 @@ QDjVuPrivate::initWidget(bool opengl)
   widget->setFocusPolicy(Qt::StrongFocus);
   widget->setSizePolicy(QSizePolicy::MinimumExpanding, 
                         QSizePolicy::MinimumExpanding);
-  // bug workaround
-#if QT_VERSION == 0x50401 && defined(Q_OS_DARWIN) 
-  opengl = 1;
-#endif
   // set opengl acceleration
 #if QT_VERSION >= 0x040400
   if (opengl)
@@ -2050,6 +2046,7 @@ QDjVuPrivate::initWidget(bool opengl)
 #endif
   vp->setAttribute(Qt::WA_NoSystemBackground);
   vp->setAttribute(Qt::WA_StaticContents);
+  vp->setAttribute(Qt::WA_NativeWindow);
   vp->setMouseTracking(true);
 }
 
