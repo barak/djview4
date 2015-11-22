645c7f23b30d61ba1494091cb283c64e2394adfd
Leon Bottou <leon@bottou.org>
2015-11-22 09:04:48 -0800

    updated win32 version

--- src/version.h
+++ src/version.h
@@ -11,10 +11,10 @@
 # define DJVIEW_APP "DjView"
 #endif
 #ifndef DJVIEW_VERSION
-# define DJVIEW_VERSION 0x40a00
-# define DJVIEW_VERSION_STR "4.10"
+# define DJVIEW_VERSION 0x40a04
+# define DJVIEW_VERSION_STR "4.10.4"
 # define RC_VERSION 4,10,0,0
-# define RC_VERSION_STR "4,10,0,0\0"
+# define RC_VERSION_STR "4,10,4,0\0"
 #endif
 
 #endif
